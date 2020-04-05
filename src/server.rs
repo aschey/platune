b635d125ec9c18b18d5dac11971fdfcd5baa83a2
Austin Schey <aschey13@gmail.com>
2020-04-04 23:18:25 -0500

    home dir

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use std::sync::mpsc;
-use actix_web::{dev::Server, HttpServer, HttpRequest, HttpResponse, App, http::Method, web};
+use dirs::home_dir;
+use std::{sync::mpsc};
+use actix_web::{dev::Server, HttpServer, HttpRequest, HttpResponse, App, http::Method, web, Result, http::StatusCode, get, error};
 use actix_files as fs;
 
 pub fn run_server(tx: mpsc::Sender<Server>) -> std::io::Result<()> {
@@ -9,13 +10,7 @@ pub fn run_server(tx: mpsc::Sender<Server>) -> std::io::Result<()> {
     let srv = HttpServer::new(|| { 
         App::new()
         .service(fs::Files::new("/music", "//home/aschey/windows/shared_files/Music").show_files_listing())
-        .service(
-            web::resource("/test").to(|req: HttpRequest| match *req.method() {
-                Method::GET => HttpResponse::Ok(),
-                Method::POST => HttpResponse::MethodNotAllowed(),
-                _ => HttpResponse::NotFound(),
-            }),
-        )
+        .service(get_home_dir)
         .service(fs::Files::new("/", "./src/ui/namp/build").show_files_listing())
         })
         .bind("127.0.0.1:5000")?
@@ -26,4 +21,15 @@ pub fn run_server(tx: mpsc::Sender<Server>) -> std::io::Result<()> {
 
     // run future
     sys.block_on(srv)
+}
+
+#[get("/homeDir")]
+async fn get_home_dir(_: HttpRequest) -> Option<Result<HttpResponse>> {
+    let home_path = home_dir()?;
+    let home_str = String::from(home_path.to_str()?);
+
+    let response = HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(home_str);
+    return Some(Ok(response));
 }
