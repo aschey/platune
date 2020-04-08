43bcf73456d3d296123da20d629bce6fec97e6cf
Austin Schey <aschey13@gmail.com>
2020-04-07 23:09:37 -0500

    toast sample

--- src/ui/namp/src/components/FolderView.tsx
+++ src/ui/namp/src/components/FolderView.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Column, Cell } from '@blueprintjs/table';
-import { Button, ITreeNode, Tooltip, Position, Icon, Classes, Intent } from '@blueprintjs/core';
+import { Button, ITreeNode, Tooltip, Position, Icon, Classes, Intent, Toaster, Toast } from '@blueprintjs/core';
 import { FolderPicker } from './FolderPicker';
 import { getJson } from '../fetchUtil';
 import { SelectedFolders } from './SelectedFolders';
+import { intentClass } from '@blueprintjs/core/lib/esm/common/classes';
+
+const AppToaster = Toaster.create({
+    position: Position.TOP
+});
+
 
 export const FolderView: React.FC<{}> = () => {
     const [rows, setRows] = useState<Array<string>>([]);
@@ -22,11 +28,14 @@ export const FolderView: React.FC<{}> = () => {
         setRows([...rows]);
     }
 
+
     return (
         <div style={{display: 'flex', alignItems: 'center'}}>
         <SelectedFolders rows={rows}/>
         <FolderPicker setSelected={setSelected}/>
-        <Button intent={Intent.PRIMARY} onClick={addFolderClick} text="Add"/>
+        <Button intent={Intent.PRIMARY} onClick={addFolderClick} icon='add' text='Add'/>
+        <Button intent={Intent.SUCCESS} icon='floppy-disk' text='Save' 
+            onClick={() => AppToaster.show({message: 'Success', intent: Intent.SUCCESS, icon: 'tick-circle', timeout: 1000})}/>
         </div>
     )
 }
