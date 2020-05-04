53558013fa9b81dd7ce942069b447edf95d921b4
Austin Schey <aschey13@gmail.com>
2020-05-04 17:13:42 -0500

    queue working

--- src/ui/namp/src/components/Audio.tsx
+++ src/ui/namp/src/components/Audio.tsx
@@ -1,19 +1,15 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import context from '../context';
 
 interface AudioProps {
     songQueue: string[],
-    setSongQueue: (queue: string[]) => void,
     onFinished: () => void
 }
-declare global {
-    interface Window { context: AudioContext; }
-}
-export const Audio: React.FC<AudioProps> = ({songQueue, setSongQueue, onFinished}) => {
+
+export const Audio: React.FC<AudioProps> = ({songQueue, onFinished}) => {
     const switchTime = useRef<number>(0);
     const index = useRef<number>(0);
     const finishCounter = useRef<number>(0);
-    //const context = useRef<AudioContext>(new AudioContext());
 
     const findStartGapDuration = (audioBuffer: AudioBuffer) => {
         // Get the raw audio data for the left & right channels.
@@ -57,7 +53,7 @@ export const Audio: React.FC<AudioProps> = ({songQueue, setSongQueue, onFinished
         return audioBuffer.duration;
     }
     
-    const load = async (song: string, context: AudioContext) => {
+    const load = useCallback(async (song: string, context: AudioContext) => {
         //const context = new AudioContext();
         const data = await fetch(song);
         const arrayBuffer = await data.arrayBuffer();
@@ -72,28 +68,25 @@ export const Audio: React.FC<AudioProps> = ({songQueue, setSongQueue, onFinished
             startGap: findStartGapDuration(audioBuffer),
             endGap: findEndGapDuration(audioBuffer)
         }
-    }
+    }, []);
 
-    const schedule = async (song: string, context: AudioContext) => {
-        const startOffset = index.current == 0 ? 0.98 : 0; // percentage - this will need to get passed in for pause/resume
+    const schedule = useCallback(async (song: string, context: AudioContext) => {
+        console.log('here');
+        const startOffset = index.current === 0 ? 0.0 : 0; // percentage - this will need to get passed in for pause/resume
         const songData = await load(`file://${song}`, context);
         let startSeconds = startOffset > 0 ? Math.round(songData.audioBuffer.duration * startOffset) : songData.startGap;
-        // if (index.current === 0) {
-        //     startSeconds = 0;
-        // }
         if (switchTime.current === 0) {
             switchTime.current = context.currentTime;
         }
         const nextSwitchTime = switchTime.current + songData.audioBuffer.duration - startSeconds;
         let start = switchTime.current === 0 ? context.currentTime : switchTime.current;
-        if (index.current === 1) {
-            start -= 0.00;
-        }
+        
         songData.source.start(start, startSeconds);
-        //console.log('start' + index.current, start, startSeconds);
+        console.log('start' + index.current, start, startSeconds);
         songData.source.stop(nextSwitchTime);
-        //console.log('stop' + index.current, nextSwitchTime);
+        console.log('stop' + index.current, nextSwitchTime);
         songData.source.addEventListener('ended', function(b) {
+            console.log('ended');
             finishCounter.current++;
             if (finishCounter.current % 2 === 1) {
                 onFinished();
@@ -102,13 +95,11 @@ export const Audio: React.FC<AudioProps> = ({songQueue, setSongQueue, onFinished
         
         switchTime.current = nextSwitchTime;
         index.current++;
-    } 
+    }, [load, onFinished]);
 
-    useEffect(() => {
-        //const context = new AudioContext();
-        //context.
-        
+    useEffect(() => {        
         const scheduleAll = async () => {
+            console.log('test');
             if (songQueue.length) {
                 // const newQueue = [
                 //     '/home/aschey/windows/shared_files/Music/Between the Buried and Me/Colors/04 Sun of Nothing.m4a',
@@ -117,11 +108,11 @@ export const Audio: React.FC<AudioProps> = ({songQueue, setSongQueue, onFinished
                 const newQueue = songQueue.slice(0);
                 while (newQueue.length) {
                     const song = newQueue.shift();
+                    console.log(song);
                     if (song) {
                         await schedule(song, context);
                     }
                 }
-                setSongQueue(newQueue);
             }
         }
         scheduleAll();
--- src/ui/namp/src/components/SongGrid.tsx
+++ src/ui/namp/src/components/SongGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Table, Cell, Column, SelectionModes, IRegion } from '@blueprintjs/table';
 import { Text, Label } from '@blueprintjs/core';
 import { getJson } from '../fetchUtil';
@@ -6,17 +6,10 @@ import { Song } from '../models/song';
 import { range, sleep } from '../util';
 import { Audio } from './Audio';
 
-declare global {
-    interface Window { soundManager: any; }
-}
-
 export const SongGrid: React.FC<{}> = () => {
     const [songs, setSongs] = useState<Song[]>([]);
-    const [, setState] = useState();
-    const [selectedRow, setSelectedRow] = useState<number>(-1);
-    const [playingRow, setPlayingRow] = useState<number>(-1);
+    const playingRow = useRef<number>(-1);
     const [songQueue, setSongQueue] = useState<string[]>([]);
-    const rowRef = useRef<number>(-1);
     const numTries = 10;
 
     const loadSongs = async () => {
@@ -40,41 +33,33 @@ export const SongGrid: React.FC<{}> = () => {
     }, []);
 
     const onSongFinished = () => {
-        let newQueue = songQueue.slice(0);
-        console.log(songs[playingRow + 2].path);
-        newQueue.push(songs[playingRow + 2].path);
-        setPlayingRow(playingRow + 1);
-        setSongQueue(newQueue);
+        setSongQueue([songs[playingRow.current + 2].path]);
+        playingRow.current++;
     }
 
-    const titleRenderer = (rowIndex: number) => {
-        return (
-            <Cell>
-                <Text>
-                    {songs[rowIndex].name}
-                </Text>
-            </Cell>
-        )
+    const onSelection = (p: IRegion[]) => {
+        if (p.length > 0 && p[0] !== null && p[0] !== undefined && p[0].rows !== null && p[0].rows !== undefined) {
+            const songIndex = p[0].rows[0];
+            playingRow.current = songIndex;
+            setSongQueue([songs[songIndex].path, songs[songIndex + 1].path]);
+        }
     }
+
     return (
         <>
-        <Table numRows={songs.length} selectionModes={SelectionModes.ROWS_AND_CELLS} forceRerenderOnSelectionChange={true} selectedRegionTransform={(region, event) => ({rows: region.rows})} onSelection={(p: IRegion[]) => {
-                if (p.length > 0 && p[0] !== null && p[0] !== undefined && p[0].rows !== null && p[0].rows !== undefined) {
-                    const songIndex = p[0].rows[0];
-                    setSelectedRow(songIndex);
-                    setPlayingRow(songIndex);
-                    rowRef.current = songIndex;
-                    setSongQueue([songs[songIndex].path, songs[songIndex + 1].path]);
-                }
-            }
-            }>
-            <Column name='title' cellRenderer={titleRenderer}/>
-            <Column name='album artist' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].albumArtist}</Text></Cell>}/>
-            <Column name='artist' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].artist}</Text></Cell>}/>
-            <Column name='album' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].album}</Text></Cell>}/>
-            <Column name='path' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].path}</Text></Cell>}/>
-        </Table>
-        <Audio songQueue={songQueue} setSongQueue={setSongQueue} onFinished={onSongFinished}/>
+            <Table 
+                numRows={songs.length} 
+                selectionModes={SelectionModes.ROWS_AND_CELLS} 
+                forceRerenderOnSelectionChange={true} 
+                selectedRegionTransform={(region, event) => ({rows: region.rows})} 
+                onSelection={onSelection}>
+                <Column name='title' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].name}</Text></Cell>}/>
+                <Column name='album artist' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].albumArtist}</Text></Cell>}/>
+                <Column name='artist' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].artist}</Text></Cell>}/>
+                <Column name='album' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].album}</Text></Cell>}/>
+                <Column name='path' cellRenderer={(rowIndex) => <Cell><Text>{songs[rowIndex].path}</Text></Cell>}/>
+            </Table>
+            <Audio songQueue={songQueue} onFinished={onSongFinished}/>
         </>
     )
 }
