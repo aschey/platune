17a8081a6f03dff82f944871dcf6107a500b557c
Austin Schey <aschey13@gmail.com>
2020-07-19 12:45:09 -0500

    get ip a different way

--- src/main.rs
+++ src/main.rs
@@ -18,20 +18,31 @@ fn stop_server(srv: actix_server::Server, t: std::thread::JoinHandle<()>) {
     let _ = t.join().unwrap();
 }
 
+#[cfg(windows)]
+fn get_ip() -> String {
+    let device_name = whoami::devicename();
+    let output = std::process::Command::new("powershell")
+    .args(&["-Command", &f!("Test-Connection {device_name} -Count 1 | Select IPV4Address")])
+    .output().unwrap();
+    let addr = regex::Regex::new(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}").unwrap().find(std::str::from_utf8(&output.stdout).unwrap()).unwrap().as_str();
+    return addr.to_owned();
+}
+
 fn main() {
     let direct = true;
     let (tx, rx) = mpsc::channel();
 
     let _ = thread::spawn(move || {
-        let distro = whoami::distro();
         let device_name = whoami::devicename();
-        let addr = pnet_datalink::interfaces().iter().find(|i| !i.is_loopback()).unwrap().ips[0].ip();
+        let addr = get_ip();
+        let distro = whoami::distro();
+        
         let server_info = f!("{distro}|{device_name}|{addr}:5000");
         let socket = std::net::UdpSocket::bind("0.0.0.0:0").unwrap();
         socket.set_broadcast(true).unwrap();
         loop {
             let server_info_bytes = server_info.as_bytes();
-            //println!("{:?}",server_info);
+            println!("{:?}",server_info);
             socket.send_to(&server_info_bytes, "255.255.255.255:34254").unwrap();
             std::thread::sleep(std::time::Duration::from_secs(5));
         }
