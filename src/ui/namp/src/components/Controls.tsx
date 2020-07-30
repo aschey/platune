26b9460e8d5306b5fc3bf8383ae67ca3a66d9ee2
Austin Schey <aschey13@gmail.com>
2020-07-30 13:47:32 -0500

    visualizer working

--- src/ui/namp/src/components/Controls.tsx
+++ src/ui/namp/src/components/Controls.tsx
@@ -3,11 +3,13 @@ import { Text, Label, ProgressBar, Intent, Button, Icon, AnchorButton } from '@b
 import { Slider, Rail, Handles, Tracks, Ticks, SliderItem, GetHandleProps, GetTrackProps } from "react-compound-slider";
 import { FlexRow } from './FlexRow';
 import { FlexCol } from './FlexCol';
-import { range, formatMs } from '../util';
+import { range, formatMs, sleep } from '../util';
 import { SongProgress } from './SongProgress';
 import { Volume } from './Volume';
 import { shadeColor } from '../themes/colorMixer';
 import { Song } from '../models/song';
+import { audioQueue } from '../audio';
+import _ from 'lodash';
 
 interface ControlProps {
     isPlaying: boolean,
@@ -22,6 +24,12 @@ interface ControlProps {
 
 export const Controls: React.FC<ControlProps> = ({ isPlaying, setIsPlaying, onPause, onPlay, onStop, progress, songMillis, playingSong }) => {
     let [coloradjust, setColorAdjust] = useState('#000000');
+    let canvasRef = React.createRef<HTMLCanvasElement>();
+
+    useEffect(() => {
+        visualizer();
+    });
+
     useEffect(() => {
         setColorAdjust(shadeColor('#92c3e0', -1 * (progress / songMillis) * 40));
     }, [progress, songMillis]);
@@ -41,11 +49,60 @@ export const Controls: React.FC<ControlProps> = ({ isPlaying, setIsPlaying, onPa
         setIsPlaying(false);
     }
 
+    const visualizer = async () => {
+        if (audioQueue.currentAnalyser) {
+            audioQueue.currentAnalyser.fftSize = 2048;
+            const bufferLength = audioQueue.currentAnalyser.frequencyBinCount;
+            const dataArray = new Uint8Array(bufferLength);
+            if (canvasRef.current) {
+                const canvasCtx = canvasRef.current.getContext('2d');
+                if (!canvasCtx) {
+                    return;
+                }
+                const width = canvasRef.current.width;
+                const height = canvasRef.current.height;
+                canvasCtx?.clearRect(0, 0, width, height);
+                requestAnimationFrame(async () => {
+                    audioQueue.currentAnalyser?.getByteFrequencyData(dataArray);
+                    const bufferLength = _.takeWhile(dataArray, d => d > 0).length;
+                    canvasCtx.fillStyle = 'rgba(0,0,0,0)';
+
+                    canvasCtx.fillRect(0, 0, width, height);
+                    canvasCtx.strokeStyle = 'rgba(43, 101, 227, 0.5)';
+                    canvasCtx.lineWidth = 2;
+                    canvasCtx.beginPath();
+                    const sliceWidth = width * 1.0 / bufferLength;
+                    let x = 0;
+                    for (var i = 0; i < bufferLength; i++) {
+
+                        var v = dataArray[i] / 128.0;
+                        var y = v * height / 2;
+
+                        if (i === 0) {
+                            canvasCtx.moveTo(x, y);
+                        } else {
+                            canvasCtx.lineTo(x, y);
+                        }
+
+                        x += sliceWidth;
+                    }
+                    canvasCtx.stroke();
+                    await sleep(200);
+                    visualizer();
+                });
+            }
+
+        }
+        else {
+            await sleep(200);
+            visualizer();
+        }
+    }
+
     return <div style={{ display: 'grid', gridTemplateRows: '10px 60px', gridTemplateColumns: '40% 20% 20% 10% 10%', minHeight: 70 }}>
         <div style={{ gridColumn: '1 / 6' }}>
             <SongProgress songMillis={songMillis} progress={progress} />
         </div>
-
         <FlexRow style={{ alignItems: 'center', marginLeft: 10 }}>
             {playingSong?.hasArt ? <img src={`http://localhost:5000/albumArt?songId=${playingSong.id}`} width={50} height={50} /> : null}
             <FlexCol style={{ paddingLeft: 10 }}>
@@ -59,7 +116,6 @@ export const Controls: React.FC<ControlProps> = ({ isPlaying, setIsPlaying, onPa
                     {playingSong?.artist}
                 </FlexRow>
             </FlexCol>
-
         </FlexRow>
         <FlexCol style={{ alignItems: 'center' }}>
             <FlexRow style={{ alignItems: 'center' }}>
@@ -71,10 +127,9 @@ export const Controls: React.FC<ControlProps> = ({ isPlaying, setIsPlaying, onPa
                 <div style={{ width: 5 }} />
                 <Button className='nofocus' intent={Intent.PRIMARY} minimal icon='fast-forward' style={{ borderRadius: '50%', width: 40, height: 40 }} />
             </FlexRow>
-
         </FlexCol>
         <FlexCol className='card' style={{ marginTop: 5, marginBottom: 5, background: 'rgba(37, 49, 59, 0.2)', borderRadius: 10 }}>
-
+            <canvas ref={canvasRef} />
         </FlexCol>
         <FlexCol>
             <FlexRow style={{ fontSize: 16, alignItems: 'center', alignSelf: 'center' }}>
@@ -82,14 +137,11 @@ export const Controls: React.FC<ControlProps> = ({ isPlaying, setIsPlaying, onPa
                 <div style={{ color: shadeColor('#92c3e0', -40) }}>/{formatMs(songMillis)}</div>
             </FlexRow>
         </FlexCol>
-
         <FlexRow style={{ fontSize: 16, minWidth: '100%', alignItems: 'center' }}>
             <Icon icon='volume-up' />
-            <FlexCol style={{ marginLeft: 10, marginRight: 10, paddingBottom: 5 }}>
+            <FlexCol style={{ marginLeft: 10, marginRight: '20%', paddingBottom: 5 }}>
                 <Volume />
-
             </FlexCol>
-
         </FlexRow>
 
 
