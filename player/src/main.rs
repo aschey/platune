113c0685ecfcc2072351228f33cc705cf31b941a
Austin Schey <aschey13@gmail.com>
2021-02-07 22:42:49 -0600

    move tasks to separate function

--- player/src/main.rs
+++ player/src/main.rs
@@ -12,9 +12,10 @@ use gstreamer::{glib, prelude::Cast, Pipeline};
 use gstreamer_app as gst_app;
 use gstreamer_audio as gst_audio;
 use player_actor::{PlayerActor, PlayerCommand};
+use player_backend::PlayerInit;
 use song_queue_actor::{QueueCommand, QueueItem, SongQueueActor};
 use song_start_actor::{SongStartActor, SongStartCommand};
-use tokio::sync::mpsc::{self, Sender};
+use tokio::sync::mpsc::{self, Receiver, Sender};
 
 use gstreamer_player_backend::GstreamerPlayer;
 use state_changed_actor::{StateChanged, StateChangedActor};
@@ -28,24 +29,57 @@ use std::{
     time::Duration,
 };
 
-#[tokio::main]
-async fn main() {
-    gst::init().unwrap();
-
-    let main_loop = glib::MainLoop::new(None, false);
-
-    let (state_tx, mut state_rx) = mpsc::channel::<StateChanged>(32);
-    let (start_tx, mut start_rx) = mpsc::channel::<SongStartCommand>(32);
-    let (queue_command_tx, mut queue_command_rx) = mpsc::channel::<QueueCommand>(32);
-    let (player_tx, mut player_rx) = mpsc::channel::<PlayerCommand>(32);
-
-    let start_tx2 = start_tx.clone();
-    let player_tx2 = player_tx.clone();
-    let player_tx3 = player_tx2.clone();
-    let player_tx4 = player_tx3.clone();
-
+fn make_queue_task(
+    player_tx_: &Sender<PlayerCommand>,
+    start_tx_: &Sender<SongStartCommand>,
+    mut queue_command_rx: Receiver<QueueCommand>,
+) {
+    let player_tx = player_tx_.clone();
+    let start_tx = start_tx_.clone();
     tokio::spawn(async move {
-        let mut player = PlayerActor::new::<GstreamerPlayer>(state_tx, player_tx);
+        let mut queue = SongQueueActor::new(player_tx, start_tx);
+        while let Some(msg) = queue_command_rx.recv().await {
+            match msg {
+                QueueCommand::SetQueue { songs } => {
+                    queue.set_queue(songs).await;
+                }
+            }
+        }
+    });
+}
+
+fn make_song_start_task(
+    player_tx_: &Sender<PlayerCommand>,
+    mut start_rx: Receiver<SongStartCommand>,
+) {
+    let player_tx = player_tx_.clone();
+    tokio::spawn(async move {
+        let mut song_start_actor = SongStartActor::new(player_tx);
+        while let Some(msg) = start_rx.recv().await {
+            match msg {
+                SongStartCommand::Schedule {
+                    nseconds,
+                    player_id,
+                } => {
+                    song_start_actor.handle(nseconds, player_id).await;
+                }
+                SongStartCommand::RecvItem { item } => {
+                    song_start_actor.recv_queue_item(item);
+                }
+            }
+        }
+    });
+}
+
+fn make_player_task<T: PlayerInit + Send + 'static>(
+    state_tx_: &Sender<StateChanged>,
+    player_tx_: &Sender<PlayerCommand>,
+    mut player_rx: Receiver<PlayerCommand>,
+) {
+    let player_tx = player_tx_.clone();
+    let state_tx = state_tx_.clone();
+    tokio::spawn(async move {
+        let mut player = PlayerActor::new::<T>(state_tx, player_tx);
         while let Some(msg) = player_rx.recv().await {
             match msg {
                 PlayerCommand::Play { id } => {
@@ -66,43 +100,43 @@ async fn main() {
             }
         }
     });
+}
 
-    tokio::spawn(async move {
-        let mut queue = SongQueueActor::new(player_tx4, start_tx2);
-        while let Some(msg) = queue_command_rx.recv().await {
-            match msg {
-                QueueCommand::SetQueue { songs } => {
-                    queue.set_queue(songs).await;
-                }
-            }
-        }
-    });
-
+fn make_state_change_task(
+    start_tx_: &Sender<SongStartCommand>,
+    mut state_rx: Receiver<StateChanged>,
+) {
+    let start_tx = start_tx_.clone();
     tokio::spawn(async move {
         let mut state_changed_actor = StateChangedActor::new(start_tx);
         while let Some(msg) = state_rx.recv().await {
             state_changed_actor.handle(msg).await;
         }
     });
+}
 
-    tokio::spawn(async move {
-        let mut song_start_actor = SongStartActor::new(player_tx3);
-        while let Some(msg) = start_rx.recv().await {
-            match msg {
-                SongStartCommand::Schedule {
-                    nseconds,
-                    player_id,
-                } => {
-                    song_start_actor.handle(nseconds, player_id).await;
-                }
-                SongStartCommand::RecvItem { item } => {
-                    song_start_actor.recv_queue_item(item);
-                }
-            }
-        }
-    });
+fn start_tasks<T: PlayerInit + Send + 'static>() -> (Sender<PlayerCommand>, Sender<QueueCommand>) {
+    let (state_tx, state_rx) = mpsc::channel::<StateChanged>(32);
+    let (start_tx, start_rx) = mpsc::channel::<SongStartCommand>(32);
+    let (queue_tx, queue_rx) = mpsc::channel::<QueueCommand>(32);
+    let (player_tx, player_rx) = mpsc::channel::<PlayerCommand>(32);
 
-    queue_command_tx
+    make_player_task::<T>(&state_tx, &player_tx, player_rx);
+    make_queue_task(&player_tx, &start_tx, queue_rx);
+    make_state_change_task(&start_tx, state_rx);
+    make_song_start_task(&player_tx, start_rx);
+
+    (player_tx, queue_tx)
+}
+
+#[tokio::main]
+async fn main() {
+    gst::init().unwrap();
+
+    let main_loop = glib::MainLoop::new(None, false);
+    let (player_tx, queue_tx) = start_tasks::<GstreamerPlayer>();
+
+    queue_tx
         .send(QueueCommand::SetQueue {
             songs: vec!["file://c/shared_files/Music/Between the Buried and Me/Colors/04 Sun Of Nothing.m4a".to_owned(),
             "file://c/shared_files/Music/Between the Buried and Me/Colors/05 Ants of the Sky.m4a".to_owned()],
@@ -110,11 +144,8 @@ async fn main() {
         .await
         .unwrap();
 
-    player_tx2
-        .send(PlayerCommand::Play { id: 0 })
-        .await
-        .unwrap();
-    player_tx2
+    player_tx.send(PlayerCommand::Play { id: 0 }).await.unwrap();
+    player_tx
         .send(PlayerCommand::Seek {
             id: 0,
             position: (60 * 10 + 58) * 1e9 as u64,
---
+++ player/src/test.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod test {
+    #[tokio::test]
+    async fn test() {
+        
+    }
+}
