913a468668e4ed3503e819fc8dd598f0579f6da6
aschey <aschey13@gmail.com>
2021-03-08 00:09:28 -0600

    remove player-old

--- player-old/src/time.rs
+++
@@ -1,7 +0,0 @@
-use gstreamer::{Clock, ClockExt, ClockTime, SystemClock};
-use lazy_static::lazy_static;
-
-lazy_static! {
-    pub static ref SYSTEM_CLOCK: Clock = SystemClock::obtain();
-    pub static ref BASE_TIME: ClockTime = SYSTEM_CLOCK.get_time();
-}
