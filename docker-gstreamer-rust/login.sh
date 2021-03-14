f46b62998bd3bfc38daa3a165ebd63a59f82f7fa
aschey <aschey13@gmail.com>
2021-03-14 16:17:20 -0500

    switch to ghcr

--- docker-gstreamer-rust/login.sh
+++ docker-gstreamer-rust/login.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo $GITHUB_TOKEN | docker login https://docker.pkg.github.com -u $GITHUB_USERNAME --password-stdin
+echo $GITHUB_TOKEN | docker login https://ghcr.io -u $GITHUB_USERNAME --password-stdin
