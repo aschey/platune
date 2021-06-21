74939c15e659ee780440cb61bae02b8d04470145
Austin Schey <aschey13@gmail.com>
2021-06-20 19:16:17 -0500

    fix connection pool

--- libplatune/management/tests/test.rs
+++ libplatune/management/tests/test.rs
@@ -15,7 +15,7 @@ use sqlx::{
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
 pub async fn test() {
-    let (db, conn) = get_db().await;
+    let db = get_db().await;
     let dir = env::temp_dir().join("platune_test1");
     let config = Config::new_from_path(&db, dir);
     config.add_folder("test1").await;
@@ -23,18 +23,20 @@ pub async fn test() {
     config.add_folder("test2").await;
     let folders = config.get_all_folders().await;
     assert_eq!(vec!["test1", "test2"], folders);
-    conn.release();
 }
 
-async fn get_db() -> (Database, PoolConnection<Sqlite>) {
+async fn get_db() -> Database {
     let opts = SqliteConnectOptions::new()
-        .filename(":memory:")
+        .filename("test.db")
+        .create_if_missing(true)
         .log_statements(LevelFilter::Debug)
         .to_owned();
 
-    let pool = SqlitePool::connect_with(opts).await.unwrap();
-    let conn = pool.acquire().await.unwrap();
+    let pool = SqlitePoolOptions::new()
+        .max_connections(1)
+        .connect_lazy_with(opts);
+
     let db = Database::from_pool(pool).await;
     db.migrate().await;
-    (db, conn)
+    db
 }
