0b8b846d20f4151464430b0ede409a91fcd80d7b
Austin Schey <aschey13@gmail.com>
2021-06-20 21:56:43 -0500

    moar tests

--- libplatune/management/tests/test.rs
+++ libplatune/management/tests/test.rs
@@ -28,6 +28,53 @@ pub async fn test_add_folders() {
     assert_eq!(vec!["test1", "test2"], folders);
 }
 
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+pub async fn test_change_mount() {
+    let tempdir = TempDir::new().unwrap();
+    let (db, config) = setup(&tempdir).await;
+
+    config.register_drive("C:\\").await;
+    config.add_folder("C:\\test").await;
+    let folders1 = config.get_all_folders().await;
+    config.register_drive("D:\\").await;
+    let folders2 = config.get_all_folders().await;
+    db.close().await;
+    assert_eq!(vec!["C:\\test"], folders1);
+    assert_eq!(vec!["D:\\test"], folders2);
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+pub async fn test_change_mount_after() {
+    let tempdir = TempDir::new().unwrap();
+    let (db, config) = setup(&tempdir).await;
+
+    config.add_folder("C:\\test").await;
+    config.register_drive("C:\\").await;
+    let folders1 = config.get_all_folders().await;
+    config.register_drive("D:\\").await;
+    let folders2 = config.get_all_folders().await;
+    db.close().await;
+    assert_eq!(vec!["C:\\test"], folders1);
+    assert_eq!(vec!["D:\\test"], folders2);
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+pub async fn test_multiple_mounts() {
+    let tempdir = TempDir::new().unwrap();
+    let (db, config) = setup(&tempdir).await;
+    let config_path2 = tempdir.path().join("platuneconfig2");
+    let config2 = Config::new_from_path(&db, config_path2);
+
+    config.add_folder("C:\\test").await;
+    config.register_drive("C:\\").await;
+    let folders1 = config.get_all_folders().await;
+    config2.register_drive("D:\\").await;
+    let folders2 = config2.get_all_folders().await;
+    db.close().await;
+    assert_eq!(vec!["C:\\test"], folders1);
+    assert_eq!(vec!["D:\\test"], folders2);
+}
+
 async fn setup(tempdir: &TempDir) -> (Database, Config) {
     let sql_path = tempdir.path().join("platune.db");
     let config_path = tempdir.path().join("platuneconfig");
