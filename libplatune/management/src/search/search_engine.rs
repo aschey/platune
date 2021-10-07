d842b27b7acc0ebbcd737b01224bcfc9712ae687
Austin Schey <aschey13@gmail.com>
2021-10-07 00:21:26 -0500

    move query text to separate functions

--- libplatune/management/src/search/search_engine.rs
+++ libplatune/management/src/search/search_engine.rs
@@ -82,6 +82,86 @@ impl SearchEngine {
         return query + "*";
     }
 
+    fn get_spellfix_query(&self, index: usize) -> String {
+        let score_clause = format!("case 
+        when word like '% %' then (distance * 1.0 / (length(word) - length(replace(word, ' ', '')))) * 3.5 
+        else editdist3(${0}, word) * 1.0 / length(word) end", index);
+        format!(
+            "
+            select * from (
+                select distinct word, ${0} search, {1} score
+                from search_spellfix 
+                where word match replace(${0}, '*', '')
+                and ({1}) <= 50
+                order by {1}
+                limit 5
+            )
+            ",
+            index, score_clause
+        )
+    }
+
+    fn get_search_query(
+        &self,
+        artist_filter: &Vec<String>,
+        restrict_entry_type: &Vec<&str>,
+    ) -> String {
+        let artist_select = "CASE entry_type WHEN 'song' THEN ar.artist_name WHEN 'album' THEN aa.album_artist_name ELSE NULL END";
+
+        let mut artist_filter_clause = "".to_owned();
+        let num_base_args = 5;
+        let mut num_extra_args = 0;
+        if artist_filter.len() > 0 {
+            let start = num_base_args + num_extra_args + 1;
+            let artist_list = (start..start + artist_filter.len())
+                .map(|i| "$".to_owned() + &i.to_string())
+                .collect_vec()
+                .join(",");
+            artist_filter_clause = format!("WHERE {} in ({})", artist_select, artist_list);
+            num_extra_args += artist_filter.len();
+        }
+        let mut type_filter = "".to_owned();
+        if !restrict_entry_type.is_empty() {
+            let start = num_base_args + num_extra_args + 1;
+            let in_list = (start..start + restrict_entry_type.len())
+                .map(|i| "$".to_owned() + &i.to_string())
+                .collect_vec()
+                .join(",");
+
+            type_filter = format!("AND entry_type in ({})", &in_list);
+        }
+
+        let full_query = format!("
+        WITH CTE AS (
+            SELECT DISTINCT entry, entry_type, rank, $1 start_highlight, $2 end_highlight, assoc_id correlation_id,
+            {0} artist,
+            al2.album_name album,
+            ROW_NUMBER() OVER (PARTITION BY 
+                entry_value, 
+                {0}, 
+                CASE entry_type WHEN 'song' THEN 1 WHEN 'album' THEN 2 WHEN 'tag' THEN 3 ELSE 4 END,
+                CASE entry_type WHEN 'song' THEN s.song_title + s.album_id WHEN 'album' THEN al.album_name WHEN 'artist' THEN ar2.artist_name WHEN 'album_artist' THEN aa2.album_artist_name END
+                ORDER BY entry_type DESC) row_num
+            FROM (select entry_type, assoc_id, entry_value, highlight(search_index, 0, '{3}', '{4}') entry, rank from search_index where entry_value match $3 {2}) a
+            LEFT OUTER JOIN song s on s.song_id = assoc_id
+            LEFT OUTER JOIN artist ar on ar.artist_id = s.artist_id
+            LEFT OUTER JOIN album al on al.album_id = assoc_id
+            LEFT OUTER JOIN album al2 on al2.album_id = s.album_id
+            LEFT OUTER JOIN album_artist aa on aa.album_artist_id = al.album_artist_id
+            LEFT OUTER JOIN artist ar2 on ar2.artist_id = assoc_id
+            LEFT OUTER JOIN album_artist aa2 on aa2.album_artist_id = assoc_id
+            {1}
+            ORDER BY rank
+            LIMIT $4
+        )
+        SELECT entry, entry_type, artist, album, correlation_id, start_highlight, end_highlight FROM cte
+        WHERE row_num = 1
+        ORDER BY rank
+        LIMIT $5", artist_select, artist_filter_clause, type_filter, START_MATCH_TEXT, END_MATCH_TEXT);
+
+        return full_query;
+    }
+
     async fn search_helper(
         &self,
         query: &str,
@@ -113,28 +193,11 @@ impl SearchEngine {
         let terms = re.split(&query).collect_vec();
         let last = terms.last().unwrap().to_owned().to_owned();
         let spellfix_query = terms
-        .iter()
-        .enumerate()
-        .map(|(i, _)| {
-            let score_clause = format!("case 
-            when word like '% %' then (distance * 1.0 / (length(word) - length(replace(word, ' ', '')))) * 3.5 
-            else editdist3(${0}, word) * 1.0 / length(word) end", i + 1);
-            format!(
-                "
-                select * from (
-                    select distinct word, ${0} search, {1} score
-                    from search_spellfix 
-                    where word match replace(${0}, '*', '')
-                    and ({1}) <= 50
-                    order by {1}
-                    limit 5
-                )
-                ",
-                i + 1, score_clause
-            )
-        })
-        .collect_vec()
-        .join(" union all ");
+            .iter()
+            .enumerate()
+            .map(|(i, _)| self.get_spellfix_query(i + 1))
+            .collect_vec()
+            .join(" union all ");
 
         let mut corrected = sqlx::query_as::<_, SpellfixResult>(&spellfix_query);
         for term in terms {
@@ -225,56 +288,8 @@ impl SearchEngine {
         artist_filter: &Vec<String>,
         con: &mut PoolConnection<Sqlite>,
     ) -> Vec<SearchEntry> {
-        let artist_select = "CASE entry_type WHEN 'song' THEN ar.artist_name WHEN 'album' THEN aa.album_artist_name ELSE NULL END";
-        let mut artist_filter_clause = "".to_owned();
-        let num_base_args = 5;
-        let mut num_extra_args = 0;
-        if artist_filter.len() > 0 {
-            let start = num_base_args + num_extra_args + 1;
-            let artist_list = (start..start + artist_filter.len())
-                .map(|i| "$".to_owned() + &i.to_string())
-                .collect_vec()
-                .join(",");
-            artist_filter_clause = format!("WHERE {} in ({})", artist_select, artist_list);
-            num_extra_args += artist_filter.len();
-        }
-        let mut type_filter = "".to_owned();
-        if !options.restrict_entry_type.is_empty() {
-            let start = num_base_args + num_extra_args + 1;
-            let in_list = (start..start + options.restrict_entry_type.len())
-                .map(|i| "$".to_owned() + &i.to_string())
-                .collect_vec()
-                .join(",");
+        let full_query = self.get_search_query(artist_filter, &options.restrict_entry_type);
 
-            type_filter = format!("AND entry_type in ({})", &in_list);
-        }
-        let full_query = format!("
-    WITH CTE AS (
-        SELECT DISTINCT entry, entry_type, rank, $1 start_highlight, $2 end_highlight, assoc_id correlation_id,
-        {0} artist,
-        al2.album_name album,
-        ROW_NUMBER() OVER (PARTITION BY 
-            entry_value, 
-            {0}, 
-            CASE entry_type WHEN 'song' THEN 1 WHEN 'album' THEN 2 WHEN 'tag' THEN 3 ELSE 4 END,
-            CASE entry_type WHEN 'song' THEN s.song_title + s.album_id WHEN 'album' THEN al.album_name WHEN 'artist' THEN ar2.artist_name WHEN 'album_artist' THEN aa2.album_artist_name END
-            ORDER BY entry_type DESC) row_num
-        FROM (select entry_type, assoc_id, entry_value, highlight(search_index, 0, '{3}', '{4}') entry, rank from search_index where entry_value match $3 {2}) a
-        LEFT OUTER JOIN song s on s.song_id = assoc_id
-        LEFT OUTER JOIN artist ar on ar.artist_id = s.artist_id
-        LEFT OUTER JOIN album al on al.album_id = assoc_id
-        LEFT OUTER JOIN album al2 on al2.album_id = s.album_id
-        LEFT OUTER JOIN album_artist aa on aa.album_artist_id = al.album_artist_id
-        LEFT OUTER JOIN artist ar2 on ar2.artist_id = assoc_id
-        LEFT OUTER JOIN album_artist aa2 on aa2.album_artist_id = assoc_id
-        {1}
-        ORDER BY rank
-        LIMIT $4
-    )
-    SELECT entry, entry_type, artist, album, correlation_id, start_highlight, end_highlight FROM cte
-    WHERE row_num = 1
-    ORDER BY rank
-    LIMIT $5", artist_select, artist_filter_clause, type_filter, START_MATCH_TEXT, END_MATCH_TEXT);
         let mut sql_query = sqlx::query(&full_query)
             .bind(options.start_highlight)
             .bind(options.end_highlight)
