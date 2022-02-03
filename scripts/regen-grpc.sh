a8ee9c338a009f29de8c73d513430b1b6f566910
Austin Schey <aschey13@gmail.com>
2022-02-03 00:57:25 -0600

    switch to cargo make

--- scripts/regen-grpc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-cargo clean -p platuned-client -p platuned
-cargo build
-echo "regenerated rust client/server"
-(cd "$(dirname $0)/../platuned/client/go" && ./gen.sh)
-(cd "$(dirname $0)/../platune-cli" && ./gen-mock.sh)
