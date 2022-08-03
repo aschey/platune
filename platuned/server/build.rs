4497a6c8c638a1d52a108a4a1572ba0fc7f1edbb
Austin Schey <aschey13@gmail.com>
2022-08-02 23:36:12 -0500

    remove other flag

--- platuned/server/build.rs
+++ platuned/server/build.rs
@@ -5,7 +5,6 @@ fn main() {
 
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("rpc_descriptor.bin"))
-        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
             &["../proto/player_rpc.proto", "../proto/management_rpc.proto"],
             &["../proto/"],
