41410f0fd2e533dd80e8fac17fdb9a1d25bda4bd
Austin Schey <aschey13@gmail.com>
2023-06-14 10:26:21 -0500

    Eagerly test connection on initial connect

--- platuned/client/go/client.go
+++ platuned/client/go/client.go
@@ -1,10 +1,20 @@
 package platune
 
 import (
+	"context"
+	"time"
+
 	grpc "google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func GetHttpClient(address string) (*grpc.ClientConn, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	// Eagerly test connection on initial connect
+	_, err := grpc.DialContext(ctx, address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		return nil, err
+	}
 	return grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 }
