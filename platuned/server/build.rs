cdac6e3633490017511ae1e2e8a1702b3ff34007
Austin Schey <aschey13@gmail.com>
2026-07-28 21:38:30 -0700

    update deps

--- platuned/server/build.rs
+++ platuned/server/build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 
-use vergen_gix::{BuildBuilder, CargoBuilder, Emitter, GixBuilder, RustcBuilder, SysinfoBuilder};
+use vergen_gix::{Build, Cargo, Emitter, Gix, Rustc, Sysinfo};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo::rerun-if-changed=../proto/*");
@@ -19,19 +19,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         )?;
 
     Emitter::default()
-        .add_instructions(&BuildBuilder::default().build_timestamp(true).build()?)?
-        .add_instructions(&CargoBuilder::default().target_triple(true).build()?)?
+        .add_instructions(&Build::builder().build_timestamp(true).build())?
+        .add_instructions(&Cargo::builder().target_triple(true).build())?
         .add_instructions(
-            &GixBuilder::default()
+            &Gix::builder()
                 .branch(true)
                 .commit_count(true)
                 .commit_timestamp(true)
                 .sha(false)
                 .describe(true, true, None)
-                .build()?,
+                .build(),
         )?
-        .add_instructions(&RustcBuilder::default().semver(true).build()?)?
-        .add_instructions(&SysinfoBuilder::default().os_version(true).build()?)?
+        .add_instructions(&Rustc::builder().semver(true).build())?
+        .add_instructions(&Sysinfo::builder().os_version(true).build())?
         .emit()?;
     Ok(())
 }
